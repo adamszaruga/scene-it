b0feca65cab18f39fc6ab02fb72cfb4d9cd6b60a
Adam Szaruga <adamszaruga@gmail.com>
2017-10-07 13:59:43 -0400

    implemented movie class, and watchlist saving

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 $(function(){
-	//  X Listen for when the form submits
-	//  X When it submits, get the value of the search box
-	//  X use $.get() to hit up the OMDb API
-	//  X empty the previous results
-	//  X loop over the results
+
 	function renderMovies(movies) {
 		$('.card-columns').empty();
 
 		movies.forEach(function(movie){
-			// for each result, generate card HTML, 
-			//     injecting movie info into the card
-			// add the card to the screen
 			
 			var newCardDiv = $('<div class="card" style="width: 20rem;"></div>');
 			var imagePoster = $('<img class="card-img-top"/>');
 			var newBodyDiv = $('<div class="card-body"></div>');
 			var newCardTitle = $('<h4 class="card-title"></h4>');
 			var newCardBadgeYear = $('<span class="badge badge-secondary float-right"></span>');
-			var newAnchor = $('<a href="#" class="btn btn-primary">Add!</a>');
-
+			var newAnchor = $('<a href="#" data-movie="'+ movie.imdbID +'" class="btn btn-primary addButton">Add!</a>');
 
 			if (movie.Poster == "N/A") {
 				imagePoster.attr('src', "no_image.png");
 			} else {
 				imagePoster.attr('src', movie.Poster);
 			}
+
 			newCardTitle.text(movie.Title);
 			newCardBadgeYear.text(movie.Year);
 
@@ -33,7 +26,7 @@ $(function(){
 			newCardDiv.append(imagePoster, newBodyDiv);
 
 			$('.card-columns').append(newCardDiv);
-		})
+		});
 	}
 
 	$('form').submit(function(e){
@@ -44,9 +37,46 @@ $(function(){
 		$.get(url, function(data){
 			renderMovies(data.Search);
 		});
-
-
 	});
 
+	// Make a  click listener on the Add buttons
+	$('.card-columns').on('click', '.addButton' ,function(e){
+		var addButton = $(this);
+		var movieID = addButton.data('movie');
+
+		var url = 'http://www.omdbapi.com/?apikey=3430a78&i=' + movieID;
+		$.get(url, function(data){
+			var movieInstance = new Movie(data);
+			movieInstance.addToWatchList();
+		})
+
+	})
+
+	// Make the watchlist populate when the modal shows
+	$('.watchlist').on('shown.bs.modal', function () {
+		var currentWatchlist = localStorage.getItem('myWatchList');
+		currentWatchlist = JSON.parse(currentWatchlist);
+		if (!currentWatchlist) {
+			currentWatchlist = {};
+		}
+
+		$('.modal-body').empty();
+
+		Object.keys(currentWatchlist).forEach(function(imdbID){
+			var currentMovieData = currentWatchlist[imdbID];
+			var movieInstance = new Movie(currentMovieData);
+
+			var movieHTML = movieInstance.generateHTML();
+
+			$('.modal-body').append(movieHTML);
+		});
+
+	})
 
 })
+
+
+
+
+
+
